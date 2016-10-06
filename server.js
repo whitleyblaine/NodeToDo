477370094bfbaefaf8d82697dbada8b92be5a118
Whitley Blaine <whitleyblaine@gmail.com>
2016-10-06 16:21:06 -0400

    Changed port and package.json for heroku

--- server.js
+++ server.js
@@ -26,5 +26,4 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/tasks_controller.js');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+app.listen(process.env.PORT || 3000);
